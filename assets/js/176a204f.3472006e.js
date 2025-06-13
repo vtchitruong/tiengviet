e7835c3ae4a8c0342aceb9a1f1a282c344378d4b
vtchitruong <vtchitruong@gmail.com>
2025-06-13 21:32:37 +0700

    Updates

---
+++ assets/js/176a204f.3472006e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktiengviet=self.webpackChunktiengviet||[]).push([[2232],{4592:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/tiengviet/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.df599278.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktiengviet=self.webpackChunktiengviet||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/tiengviet/blog","blogTitle":"Blog","authorsListPath":"/tiengviet/blog/authors"}')}}]);
